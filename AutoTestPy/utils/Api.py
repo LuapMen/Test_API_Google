fa7c91148009e948fe0a40783f014920d6afc203
Roman Kravchenko <crav33@gmail.com>
2023-06-06 21:35:31 +0000

    commit add utils

---
+++ AutoTestPy/utils/Api.py
@@ -0,0 +1,8 @@
+from utils.http_methods import Http_methods
+
+""""""
+
+class Google_maps_api():
+
+    @staticmethod
+    def create_new_place():
---
+++ AutoTestPy/utils/http_methods.py
@@ -0,0 +1,27 @@
+import requests
+
+"""List HTTP METHODS"""
+class Http_methods:
+    headers = {'Content-Type': 'application/json'}
+    cookeis = ""
+
+    @staticmethod
+    def get(url):
+            result = requests.get(url, headers=Http_methods.headers, cookeis=Http_methods.cookeis)
+            return result
+
+
+    @staticmethod
+    def post(url,body):
+            result = requests.post(url,json=body, headers=Http_methods.headers, cookeis=Http_methods.cookeis)
+            return result
+
+    @staticmethod
+    def put(url, body):
+        result = requests.put(url, json=body, headers=Http_methods.headers, cookeis=Http_methods.cookeis)
+        return result
+
+    @staticmethod
+    def delete(url, body):
+        result = requests.delete(url, json=body, headers=Http_methods.headers, cookeis=Http_methods.cookeis)
+        return result
